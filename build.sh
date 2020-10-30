f92d5579db8d7e4be844d52e1a3052ea5d45f7c7
Jun-Wei Mao <twjohn1206@gmail.com>
2020-10-30 16:16:07 +0800

    Add release shell

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+project="sftp-JSch-wrapper"
+currentDir=$(pwd)
+
+if [[ "$currentDir" != *"$project"* ]]; then
+    echo "Need to execute build.sh at project folder !"
+    read -rp "Is '$currentDir' the project folder? " answer
+    case $answer in
+        [Yy]* ) ;;
+        [Nn]* ) echo "exit now." & exit;;
+    esac
+fi
+
+currentVer=$(less VERSION)
+
+read -erp "Version for this build? " -i "$currentVer" newVer
+
+echo ""
+echo "[START] Gradle clean Ver.$newVer"
+gradle clean -Pversion="$newVer"
+echo ""
+
+echo ""
+echo "[START] Gradle build Ver.$newVer"
+gradle build -Pversion="$newVer"
+echo ""
+
+if [ -f "VERSION" ]; then
+    echo "$newVer" > VERSION
+fi
+
+[[ -d "out" ]] || mkdir "out"
+
+mv "build/libs/$project-$currentVer.jar" "out/$project-$currentVer.jar"
